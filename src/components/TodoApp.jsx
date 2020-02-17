6fbb404d056bc621edd66d53aa3aec883c88369c
Marvin Dawson <marvintdawson@gmail.com>
2020-02-16 23:44:35 -0600

    added Todo Component

---
+++ src/components/TodoApp.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class TodoApp extends Component {
+    render() {
+        return (
+            <div className="TodoApp">
+                My Todo Application
+            </div>
+        );
+    }
+}
+
+export default TodoApp
